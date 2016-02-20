6ed1142073f7e6e1c620475220f1f4a200962a66
shrekshao <shrekshao@gmail.com>
2016-02-19 19:24:00 -0500

    MouseInputStruct

---
+++ Assets/Scripts/MouseInputStruct.cs
@@ -0,0 +1,33 @@
+﻿//using UnityEngine;
+using System.Collections;
+
+namespace Swordsmanship
+{
+    public enum MouseMovementsInput
+    {
+        Idle,
+
+        Attack_SwingLeft,
+        Attack_SwingRight,
+        Attack_Stab,
+
+        Block_Front,
+        Block_Left,
+        Block_Right,
+
+        Special_DragonRoar
+    };
+
+    public class MouseInputStruct
+    {
+        public MouseMovementsInput mouseMovementInput;
+
+        //TODO: speed, pattern completions...
+
+        MouseInputStruct()
+        {
+
+        }
+    }
+}
+
