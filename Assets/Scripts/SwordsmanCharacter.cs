9393fbf95c3d0e594d939920b020188e76de4bbd
shrekshao <shrekshao@gmail.com>
2016-02-19 23:02:50 -0500

    fix grounded, change animator

--- Assets/Scripts/SwordsmanCharacter.cs
+++ Assets/Scripts/SwordsmanCharacter.cs
@@ -80,51 +80,51 @@ namespace Swordsmanship
 				HandleAirborneMovement();
 			}
 
-			ScaleCapsuleForCrouching(crouch);
-			PreventStandingInLowHeadroom();
+			//ScaleCapsuleForCrouching(crouch);
+			//PreventStandingInLowHeadroom();
 
 			// send input and other state parameters to the animator
 			UpdateAnimator(move);
 		}
 
 
-		void ScaleCapsuleForCrouching(bool crouch)
-		{
-			if (m_IsGrounded && crouch)
-			{
-				if (m_Crouching) return;
-				m_Capsule.height = m_Capsule.height / 2f;
-				m_Capsule.center = m_Capsule.center / 2f;
-				m_Crouching = true;
-			}
-			else
-			{
-				Ray crouchRay = new Ray(m_Rigidbody.position + Vector3.up * m_Capsule.radius * k_Half, Vector3.up);
-				float crouchRayLength = m_CapsuleHeight - m_Capsule.radius * k_Half;
-				if (Physics.SphereCast(crouchRay, m_Capsule.radius * k_Half, crouchRayLength, ~0, QueryTriggerInteraction.Ignore))
-				{
-					m_Crouching = true;
-					return;
-				}
-				m_Capsule.height = m_CapsuleHeight;
-				m_Capsule.center = m_CapsuleCenter;
-				m_Crouching = false;
-			}
-		}
+		//void ScaleCapsuleForCrouching(bool crouch)
+		//{
+		//	if (m_IsGrounded && crouch)
+		//	{
+		//		if (m_Crouching) return;
+		//		m_Capsule.height = m_Capsule.height / 2f;
+		//		m_Capsule.center = m_Capsule.center / 2f;
+		//		m_Crouching = true;
+		//	}
+		//	else
+		//	{
+		//		Ray crouchRay = new Ray(m_Rigidbody.position + Vector3.up * m_Capsule.radius * k_Half, Vector3.up);
+		//		float crouchRayLength = m_CapsuleHeight - m_Capsule.radius * k_Half;
+		//		if (Physics.SphereCast(crouchRay, m_Capsule.radius * k_Half, crouchRayLength, ~0, QueryTriggerInteraction.Ignore))
+		//		{
+		//			m_Crouching = true;
+		//			return;
+		//		}
+		//		m_Capsule.height = m_CapsuleHeight;
+		//		m_Capsule.center = m_CapsuleCenter;
+		//		m_Crouching = false;
+		//	}
+		//}
 
-		void PreventStandingInLowHeadroom()
-		{
-			// prevent standing up in crouch-only zones
-			if (!m_Crouching)
-			{
-				Ray crouchRay = new Ray(m_Rigidbody.position + Vector3.up * m_Capsule.radius * k_Half, Vector3.up);
-				float crouchRayLength = m_CapsuleHeight - m_Capsule.radius * k_Half;
-				if (Physics.SphereCast(crouchRay, m_Capsule.radius * k_Half, crouchRayLength, ~0, QueryTriggerInteraction.Ignore))
-				{
-					m_Crouching = true;
-				}
-			}
-		}
+		//void PreventStandingInLowHeadroom()
+		//{
+		//	// prevent standing up in crouch-only zones
+		//	if (!m_Crouching)
+		//	{
+		//		Ray crouchRay = new Ray(m_Rigidbody.position + Vector3.up * m_Capsule.radius * k_Half, Vector3.up);
+		//		float crouchRayLength = m_CapsuleHeight - m_Capsule.radius * k_Half;
+		//		if (Physics.SphereCast(crouchRay, m_Capsule.radius * k_Half, crouchRayLength, ~0, QueryTriggerInteraction.Ignore))
+		//		{
+		//			m_Crouching = true;
+		//		}
+		//	}
+		//}
 
 
 		void UpdateAnimator(Vector3 move)
@@ -138,6 +138,10 @@ namespace Swordsmanship
 			{
 				m_Animator.SetFloat("Jump", m_Rigidbody.velocity.y);
 			}
+            else
+            {
+                m_Animator.SetFloat("Jump", 0.0f);
+            }
 
 			// calculate which leg is behind, so as to leave that leg trailing in the jump animation
 			// (This code is reliant on the specific run cycle offset in our animations,
@@ -177,10 +181,13 @@ namespace Swordsmanship
 
 		void HandleGroundedMovement(bool crouch, bool jump)
 		{
-			// check whether conditions are right to allow a jump:
-			if (jump && !crouch && (m_Animator.GetCurrentAnimatorStateInfo(0).IsName("Grounded")
-                || m_Animator.GetCurrentAnimatorStateInfo(0).IsName("GroundedDefault")) )
-			{
+            // check whether conditions are right to allow a jump:
+
+            //if (jump && !crouch && (m_Animator.GetCurrentAnimatorStateInfo(0).IsName("Grounded")
+            //    || m_Animator.GetCurrentAnimatorStateInfo(0).IsName("GroundedDefault")) )
+
+            if (jump && !crouch && m_Animator.GetCurrentAnimatorStateInfo(0).IsName("GroundedDefault"))
+            {
 				// jump!
 				m_Rigidbody.velocity = new Vector3(m_Rigidbody.velocity.x, m_JumpPower, m_Rigidbody.velocity.z);
 				m_IsGrounded = false;
