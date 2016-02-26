ddca9b4d0b33aa3657340463eabd13f49770a97c
Liang Peng <itoupeter@gmail.com>
2016-02-26 00:08:52 -0500

    adding special

---
+++ Assets/DestroyByTime.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByTime : MonoBehaviour {
+
+    public float life;
+        
+	void Start () {
+
+        StartCoroutine( destroyByTime() );
+	}
+	
+	IEnumerator destroyByTime() {
+        
+        yield return new WaitForSeconds( life );
+        Destroy( gameObject );
+    }
+}
