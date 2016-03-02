6318dea947387e6cabe63395bc5ee056255413ed
Liang Peng <itoupeter@gmail.com>
2016-03-01 23:43:11 -0500

    fix special position

--- Assets/Scripts/MouseInput_Script/SpecialSystem.cs
+++ Assets/Scripts/MouseInput_Script/SpecialSystem.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class SpecialSystem : MonoBehaviour {
-
+    
     SpecialController[] specialController;
 
 	void Start () {
@@ -10,8 +10,11 @@ public class SpecialSystem : MonoBehaviour {
         specialController[ 0 ] = GameObject.FindGameObjectWithTag( "Special1Controller" ).GetComponent< SpecialController >();
         specialController[ 1 ] = GameObject.FindGameObjectWithTag( "Special2Controller" ).GetComponent< SpecialController >();
         specialController[ 2 ] = GameObject.FindGameObjectWithTag( "Special3Controller" ).GetComponent< SpecialController >();
+                
+        //---initialize position---
+        gameObject.transform.localPosition = new Vector3( -35.0f + ( 961.0f - Screen.width ) * 0.0413f, 18, 50 );
 	}
-	
+
     public void updateCooldown( int a, float b ) {
         specialController[ a ].coolDownPercent = b;
     }
