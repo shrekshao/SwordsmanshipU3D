3072ce68f2b300f430904c64a713ff87335ea827
zimengyang <zmyang2011@gmail.com>
2016-03-22 18:01:58 -0400

    add namespace to Special move

--- Assets/Scripts/MouseInput_Script/BurningGroundBehavior.cs
+++ Assets/Scripts/MouseInput_Script/BurningGroundBehavior.cs
@@ -1,25 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BurningGroundBehavior : MonoBehaviour {
-    
-    private GameObject attacker;
+namespace Swordsmanship
+{
+	public class BurningGroundBehavior : MonoBehaviour {
+	    
+	    private GameObject attacker;
 
-	public void dealProlongedDamage( GameObject _attacker ) {
-        
-        attacker = _attacker;
-        StartCoroutine( damage() );
-    }
+		public void dealProlongedDamage( GameObject _attacker ) {
+	        
+	        attacker = _attacker;
+	        StartCoroutine( damage() );
+	    }
 
-    IEnumerator damage() {
+	    IEnumerator damage() {
 
-        GameObject[] enemies = EnemyUtil.GetEnemies();
+	        GameObject[] enemies = EnemyUtil.GetEnemies();
 
-        for( int i = 0; i < 3; ++i ) {
-            foreach( GameObject enemy in enemies ) {
-                yield return new WaitForSeconds( 1 );
-                enemy.GetComponent< Swordsmanship.SwordsmanCharacter >().BeHit( attacker, 160 );
-            }
-        }
-    }
+	        for( int i = 0; i < 3; ++i ) {
+	            foreach( GameObject enemy in enemies ) {
+	                yield return new WaitForSeconds( 1 );
+	                enemy.GetComponent< Swordsmanship.SwordsmanCharacter >().BeHit( attacker, 160 );
+	            }
+	        }
+	    }
+	}
 }
